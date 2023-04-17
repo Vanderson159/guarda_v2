cf6ddda267024217b4b9f799612e24d883ae6cd9
Vanderson Duarte Maciel <78940842+Vanderson159@users.noreply.github.com>
2023-04-17 20:00:59 -0300

    removendo comentarios

--- lib/data/base_url.dart
+++ lib/data/base_url.dart
@@ -1,4 +1,4 @@
-const baseHost = 'https://6e96-186-251-193-214.ngrok-free.app/';
+const baseHost = 'https://7f1f-186-251-193-214.ngrok-free.app/';
 const baseUrl = '${baseHost}guardaWEBapi/api';
 const baseUrlLogin = '${baseHost}guardaWEBapi/api/login';
 const baseUrlRegister = '${baseHost}guardaWEBapi/api/register';
@@ -9,16 +9,3 @@ const baseUrlQrcodeInsert = '${baseHost}guardaWEBapi/api/insertQrcode';
 const baseUrlQrcodBuscarIdImg = '${baseHost}guardaWEBapi/api/buscarIdImage';
 const baseUrlQrcodBuscarQrCode = '${baseHost}guardaWEBapi/api/buscarQrcode';
 const baseUrlArquivar = '${baseHost}guardaWEBapi/api/arquivar';
-
-
-/*
-const baseUrl = 'http://192.168.1.117/guardaWEBapi/api';
-const baseUrlLogin = 'http://192.168.1.117/guardaWEBapi/api/login';
-const baseUrlRegister = 'http://192.168.1.117/guardaWEBapi/api/register';
-const baseUrlOcorrencia = 'http://192.168.1.117/guardaWEBapi/api/ocorrencias';
-const baseUrlImagem = 'http://192.168.1.117/guardaWEBapi/api/imagens';
-const baseUrlOcorrencias = 'http://192.168.1.117/guardaWEBapi/api/ocorrencias';
-const baseUrlQrcodeInsert = 'http://192.168.1.117/guardaWEBapi/api/insertQrcode';
-const baseUrlQrcodBuscarIdImg = 'http://192.168.1.117/guardaWEBapi/api/buscarIdImage';
-const baseUrlQrcodBuscarQrCode = 'http://192.168.1.117/guardaWEBapi/api/buscarQrcode'
-*/
